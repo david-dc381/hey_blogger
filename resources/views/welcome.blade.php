1741923cfa8f439aa4aacf482834052a323b8c10
mynombre <david_dc381@hotmail.com>
2021-03-31 20:32:42 -0400

    Se cambio el titulo de bienvenida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
         <div class="container">
             <a class="navbar-brand text-white" href="{{ url('/') }}">
                 {{-- {{ config('app.name', 'Laravel') }} --}}
-                Hey Blogger :)
+                Hey Bloggerrrrrrrrrrrrrrrrr :)
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
