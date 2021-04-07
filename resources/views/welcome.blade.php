c6885057c2ca3c0d8c506367244cee0cd03c023b
mynombre <david_dc381@hotmail.com>
2021-04-07 15:28:34 -0400

    Se añadio media queries a la vista welcome en el css | Se añadio parametros a la etiqueta meta de viewport en welcome.blade.html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}" />
     <title>Hey Blogger :)</title>
 
